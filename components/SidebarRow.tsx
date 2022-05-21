5671275306997abf35a160e244532e51548e9ca4
Matthew T Leiman <matthewtleiman@gmail.com>
2022-05-21 09:58:11 -0600

    Initial commit

---
+++ components/SidebarRow.tsx
@@ -0,0 +1,17 @@
+import React, {SVGProps} from 'react'
+
+interface Props{
+    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+    title: string
+}
+
+function SidebarRow({Icon, title}: Props) {
+  return (
+    <div>
+        <Icon className='h-6 w-6' />
+        <p>{title}</p>
+    </div>
+  )
+}
+
+export default SidebarRow
