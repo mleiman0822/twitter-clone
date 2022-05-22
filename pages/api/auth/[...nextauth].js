162a1e303cd317d6cd4d9ea16c312b26e6500db0
Matthew T Leiman <matthewtleiman@gmail.com>
2022-05-22 11:23:56 -0600

    changes

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,8 @@ export default NextAuth({
       clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: '2.0', // opting in for twitter oauth 2.0 
     }),
+    
     // ...add more providers here
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
 })
